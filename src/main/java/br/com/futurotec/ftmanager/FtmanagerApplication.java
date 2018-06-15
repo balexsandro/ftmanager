6d3dda9c8421c327715b50731b86516a0bf052dd
Breno Alexsandro <balexsandro@gmail.com>
2018-06-15 17:05:06 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/futurotec/ftmanager/FtmanagerApplication.java
@@ -0,0 +1,12 @@
+package br.com.futurotec.ftmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtmanagerApplication.class, args);
+	}
+}
